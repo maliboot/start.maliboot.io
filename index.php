9d81919f2d820fb8f15ead8e23ada43630cbebab
stone <shilei_zhang@163.com>
2023-06-15 17:26:39 +0800

    fix(master): 修复空依赖传参导致的配置异常

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ use Hyperf\Nano\ContainerProxy;
 use Hyperf\Nano\Factory\AppFactory;
 
 require_once __DIR__ . '/vendor/autoload.php';
-
 $app = AppFactory::create();
 
 $app->get('/', function () {
@@ -20,7 +19,7 @@ $app->get('/', function () {
 
 $app->get('/starter.zip', function () {
     /** @var ContainerProxy $this */
-    $queryParams = [
+    $qry = [
         'version' => $this->request->input('version', '1.x'),
         'vendorName' => $this->request->input('vendorName', 'maliboot'),
         'packageName' => $this->request->input('packageName', 'maliboot-skeleton'),
@@ -30,30 +29,40 @@ $app->get('/starter.zip', function () {
         'requireDev' => $this->request->input('requireDev', ''),
         'require' => $this->request->input('require', ''),
     ];
-    $tplDir = sprintf("%s/storage/maliboot-template/%s", BASE_PATH, $queryParams['version']);
-    $queryParamsBuildStr = md5(http_build_query($queryParams));var_dump($queryParamsBuildStr);
-    $cacheComposerFile = sprintf("%s/%s.%s", $tplDir, $queryParamsBuildStr, 'json');
-    if (file_exists($cacheComposerFile)) {
-        $tplComposerDataTxt = file_get_contents($cacheComposerFile);
-    } else {
-        $tplComposerData = json_decode(file_get_contents($tplDir . '/composer.json'), true);
-        $tplComposerData['name'] = sprintf("%s/%s", $queryParams['vendorName'], $queryParams['packageName']);
-        $tplComposerData['description'] = $queryParams['desc'];
-        $tplComposerData['require']['php'] = ">=" . $queryParams['phpVersion'];
-        $tplComposerData['repositories']['packagist']['url'] = $queryParams['packagistUrl'];
-        $queryParams['require'] && $tplComposerData['require'] = array_merge($tplComposerData['require'], array_map(function ($item) {
-            return explode(":", $item);
-        }, explode(',', $queryParams['require'])));
-        $queryParams['requireDev'] && $tplComposerData['require-dev'] = array_merge($tplComposerData['require-dev'], array_map(function ($item) {
-            return explode(":", $item);
-        }, explode(',', $queryParams['requireDev'])));
+    $tplDir = sprintf("%s/storage/maliboot-template/%s", BASE_PATH, $qry['version']);
+    $qryBuildStr = md5(http_build_query($qry));
+    $cacheComposerFile = sprintf("%s/%s.%s", $tplDir, $qryBuildStr, 'json');
 
-        $tplComposerDataTxt = json_encode($tplComposerData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    // 配置composer.json
+    if (file_exists($cacheComposerFile)) {
+        $composerTxt = file_get_contents($cacheComposerFile);
+    } else {
+        $composer = json_decode(file_get_contents($tplDir . '/composer.json'), true);
+        $composer['name'] = sprintf("%s/%s", $qry['vendorName'], $qry['packageName']);
+        $composer['description'] = $qry['desc'];
+        $composer['require']['php'] = ">=" . $qry['phpVersion'];
+        $composer['repositories']['packagist']['url'] = $qry['packagistUrl'];
+        $qry['require'] && $composer['require'] = array_merge(
+            $composer['require'],
+            array_reduce(explode(',', $qry['require']), function ($carry, $item) {
+                empty($item) || $carry[] = explode(":", $item);
+                return $carry;
+            }, [])
+        );
+        $qry['requireDev'] && $composer['require-dev'] = array_merge(
+            $composer['require-dev'],
+            array_reduce(explode(',', $qry['requireDev']), function ($carry, $item) {
+                empty($item) || $carry[] = explode(":", $item);
+                return $carry;
+            }, [])
+        );
+        $composerTxt = json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
     }
 
+    // 打压缩包输出到浏览器
     $zip = new ZipArchive();
     $zip->open($tplDir . '/demo.zip');
-    $zip->addFromString("composer.json", $tplComposerDataTxt);
+    $zip->addFromString("composer.json", $composerTxt);
     $zip->close();
     return $this->response->download($tplDir . '/demo.zip');
 });
